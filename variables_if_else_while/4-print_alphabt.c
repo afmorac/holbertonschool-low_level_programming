7857b9d304366cf849ab62a2bbb915caacc5afc1
afmorac <andresfmora.catro@gmail.com>
2025-02-23 00:23:25 -0400

    test1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase, except 'q' ans 'e'
+ *
+ * Return: Always 0 (Succedd)
+ */
+int main(void)
+{
+char *alphabet = "abcdfghijklmnoprstuvwxyz\n";
+putchar(*alphabet);
+putchar(\n);
+return (0);
+}
+
