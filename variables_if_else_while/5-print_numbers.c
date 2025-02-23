7ce018cc880000543ced89b92701613249b9b03c
afmorac <andresfmora.catro@gmail.com>
2025-02-23 00:44:22 -0400

    test1 for 4 4 4

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits # of base 10
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf('\n');
+return (0);
+}
