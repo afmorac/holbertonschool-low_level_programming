df0bdda29681fb79a2466c0ee04306fc7312f000
afmorac <andresfmora.catro@gmail.com>
2025-02-23 00:57:42 -0400

    test1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print singles digits base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
