a0477f3b55511d7406ca0441712c05a5cfd0132e
afmorac <andresfmora.catro@gmail.com>
2025-02-22 22:50:32 -0400

    test5 betty gracias!!!!!!!!!!

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2; /* Asigna un número aleatorio a n */
 
     /* Evaluar si es positivo, negativo o cero */
-    	if (n > 0)
-    printf("%d is positive\n", n);
-    else if (n == 0)
-    printf("%d is zero\n", n);
-    else
-    printf("%d is negative\n", n);
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
 
-return (0);
+	return (0);
 }
 
