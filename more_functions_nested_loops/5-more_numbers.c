284024b72def0bed5ff58d4369f356961ca440c4
afmorac <andresfmora.catro@gmail.com>
2025-02-24 21:36:54 -0400

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 0-14 ten times
+ */
+void more_numbers(void)
+{
+int i, j;
+char numbers[] = "01234567891011121314";
+
+for (i = 0; i < 10; i++)
+{
+char *ptr = numbers;
+while (*ptr)
+_putchar(*ptr++);
+_putchar('\n');
+}
+}
+
