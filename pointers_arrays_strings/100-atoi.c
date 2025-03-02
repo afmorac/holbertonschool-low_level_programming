9e9101849c4b3e85bd37eb42a7a98e37b6a3a583
afmorac <andresfmora.catro@gmail.com>
2025-03-02 17:57:44 -0400

    betty (()()(2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,8 +15,8 @@ if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-if (result > (INT_MAX / 10) ||
-result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10))
+if ((result > (INT_MAX / 10)) ||
+((result == (INT_MAX / 10)) && ((s[i] - '0') > (INT_MAX % 10))))
 {
 return (sign == 1 ? INT_MAX : INT_MIN);
 }
