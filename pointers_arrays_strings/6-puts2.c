b28058dfb2d1333e61730c74cc3f06eff6e34c0e
afmorac <andresfmora.catro@gmail.com>
2025-03-02 14:55:50 -0400

    fix output 3

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
 void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '\0' && str[i + 1] != '\0') // Evita que i+2 pase el límite
+while (str[i] != '\0' && str[i + 1] != '\0')
 {
 _putchar(str[i]);
 i += 2;
