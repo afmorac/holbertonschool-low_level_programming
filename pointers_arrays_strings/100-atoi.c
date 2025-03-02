7191a572d783d8405abd9c083e04f80f6ed61afc
afmorac <andresfmora.catro@gmail.com>
2025-03-02 17:45:05 -0400

    main max min

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 /**
  * _atoi - Converts a string to an integer
  * @s: Pointer to the string
@@ -13,10 +13,12 @@ while (s[i] != '\0')
 {
 if (s[i] == '-')
 sign *= -1;
-else if (s[i] == '+')
-sign *= 1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
+if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
+}
 result = result * 10 + (s[i] - '0');
 started = 1;
 }
