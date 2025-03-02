63271ae6c583eb9972c0903fe9faf4493a2d71d0
afmorac <andresfmora.catro@gmail.com>
2025-03-02 15:22:20 -0400

    fix output6??

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,12 @@
 void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '\0' && str[i] >= 32 && str[i] <= 126)
+while (str[i] != '\0')
 {
 _putchar(str[i]);
+if (str[i + 1] == '\0')
+break;
 i = i + 2;
 }
 _putchar('\n');
 }
-
