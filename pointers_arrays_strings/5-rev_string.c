946f012e271ae1dd75f4a0482da596dc7c2d968c
afmorac <andresfmora.catro@gmail.com>
2025-03-02 01:58:43 -0400

    test1

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src, including '\0', to dest
- * @dest: Buffer to copy to
- * @src: String to copy
- *
- * Return: Pointer to dest
+ * rev_string - Reverses a string in place
+ * @s: Pointer to the string to reverse
  */
-char *_strcpy(char *dest, char *src)
+void rev_string(char *s)
 {
-int i = 0;
-while (src[i] != '\0')
+int i = 0, j;
+char temp;
+while (s[i] != '\0')
 {
-dest[i] = src[i];
 i++;
 }
-dest[i] = '\0';
-return (dest);
+j = i - 1;
+
+i = 0;
+while (i < j)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
+}
 }
