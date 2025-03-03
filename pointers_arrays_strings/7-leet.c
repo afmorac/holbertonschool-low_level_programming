051b3c6d2db41b14d1da8f8d6f8c05a1387fc616
afmorac <andresfmora.catro@gmail.com>
2025-03-03 13:38:49 -0400

    sintax fix == letters[]

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,9 +13,9 @@ char letters[] = "aAeEoOtTlL";
 char leet[] = "4433007711";
 for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; str[j] != '\0'; j++)
+for (j = 0; letters[] != '\0'; j++)
 {
-if (str[j] = letters[j])
+if (str[j] == letters[j])
 {
 str[i] = leet[j];
 break;
