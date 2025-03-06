5386ec516272042d449a5f49ebca541a88a64e82
afmorac <andresfmora.catro@gmail.com>
2025-03-06 10:11:44 -0400

    test4

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,14 @@
  *
  * Return: El último dígito del número
  */
-int main(void)
+int print_last_digit(int n)
 {
-int r;
-r = print_lastDigit(98);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+int lasr_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+{
+last_digit = -last_digit;
+}
+_putchar(last_digit + '0')
+return (last_digit);
 }
