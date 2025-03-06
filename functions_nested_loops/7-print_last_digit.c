c80e87bb363e20bad8aab5260c72fb7974c9ac95
afmorac <andresfmora.catro@gmail.com>
2025-03-06 10:15:05 -0400

    test5

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
  */
 int print_last_digit(int n)
 {
-int lasr_digit;
+int last_digit;
 last_digit = n % 10;
 if (last_digit < 0)
 {
 last_digit = -last_digit;
 }
-_putchar(last_digit + '0')
+_putchar(last_digit + '0');
 return (last_digit);
 }
