e7317a2442e1edb3f3082f613c6a3ae8f27b704b
afmorac <andresfmora.catro@gmail.com>
2025-03-06 09:56:29 -0400

    test last #2

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,13 @@
  *
  * Return: El último dígito del número
  */
-int print_last_digit()
+int print_last_digit(int n)
 {
 int last_digit;
 last_digit = n % 10;
 if (last_digit < 0)
 {
-last_digit = last_digit
+last_digit = last_digit;
 _putchar(last_digit + '0');
 }
 return (last_digit);
