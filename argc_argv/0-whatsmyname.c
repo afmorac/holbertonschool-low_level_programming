ad50ada10f70674891a5f23ba2a78d09bc185066
afmorac <andresfmora.catro@gmail.com>
2025-03-10 13:23:12 -0400

    say my name2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused), char *argv[])
+int main(int argc, int *argv[])
 {
+(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
