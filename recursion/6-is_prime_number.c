d0871ff156ab7430d1b4bbb42528749fc0a0d3ab
afmorac <andresfmora.catro@gmail.com>
2025-03-09 22:23:07 -0400

    prime num

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: The number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n < 2)
+return (0);
+return (is_prime_helper(n, 2));
+}
+
