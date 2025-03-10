6f4d1126ebd91185a686824d7a8278c2b6003b02
afmorac <andresfmora.catro@gmail.com>
2025-03-09 20:55:46 -0400

    putchar fix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,5 +14,11 @@ for (i = 0; i < size; i++)
 sum1 += a[i * size + i];
 sum2 += a[i * size + (size - 1 - i)];
 }
-printf("%d, %d\n", sum1, sum2);
+_putchar((sum1 / 10) + '0');
+_putchar((sum1 % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar((sum2 / 10) + '0');
+_putchar((sum2 % 10) + '0');
+_putchar('\n');
 }
