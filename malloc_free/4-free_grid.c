b14c1f4c4dfe27afe62d69a6bec13853372a027e
afmorac <andresfmora.catro@gmail.com>
2025-03-12 13:23:59 -0400

    grid free3

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,27 +2,21 @@
 #include <stdlib.h>
 
 /**
- * create_array - Crea un array de caracteres e inicializa
- * cada posición con un carácter específico.
- * @size: Tamaño del array.
- * @c: Carácter con el que se inicializará el array.
+ * free_grid - Libera una cuadrícula bidimensional de enteros.
+ * @grid: Puntero a la cuadrícula.
+ * @height: Número de filas de la cuadrícula.
  *
- * Return: Puntero al array, o NULL si falla la asignación de memoria.
+ * Return: Nada.
  */
-char *create_array(unsigned int size, char c)
+void free_grid(int **grid, int height)
 {
-char *arr;
-unsigned int i;
+int i;
 
-if (size == 0)
-return (NULL);
+if (grid == NULL || height <= 0)
+return;
 
-arr = malloc(size * sizeof(char));
-if (arr == NULL)
-return (NULL);
+for (i = 0; i < height; i++)
+free(grid[i]);
 
-for (i = 0; i < size; i++)
-arr[i] = c;
-
-return (arr);
+free(grid);
 }
